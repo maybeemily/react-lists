f56026b0503d68454ce0af2dd03a0077bff16a6d
emily baier <emilyrbaier@gmail.com>
2019-05-16 11:02:16 -0700

    sidebar is doing the sidebar thing

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Sidebar.css';
+
+function Sidebar({ children }) {
+  return (
+    <nav className={styles.Sidebar}>
+      {children}
+    </nav> 
+  );
+}
+
+Sidebar.propTypes = {
+  children: PropTypes.node
+};
+
+export default Sidebar;
